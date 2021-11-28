d91533439dfb48e7615f1d0e1e44150fceb64629
wogur311 <wogur311@naver.com>
2021-11-28 18:04:36 +0900

    일시정지오류해결

--- shooting_game.py
+++ shooting_game.py
@@ -4,6 +4,7 @@ from collections import deque
 
 from pygame.constants import SCALED, VIDEORESIZE
 from pygame import Surface, draw
+from pymysql import NULL
 
 
 from sprites import (MasterSprite, Ship, Alien, Missile, BombPowerup,
@@ -581,7 +582,7 @@ def main():
             elif (event.type == pygame.KEYUP
                   and event.key in direction.keys()):
                 ship.horiz -= direction[event.key][0] * speed
-                ship.vert -= direction[event.key][1] * speed
+                ship.vert -= direction[event.key][1] * speed            
             elif (event.type == pygame.KEYDOWN
                   and event.key == pygame.K_SPACE):
                 time = 0
@@ -599,6 +600,8 @@ def main():
                         bomb_sound.play()
             elif (event.type == pygame.KEYDOWN
                     and event.key == pygame.K_p): ####### 일시정지 ######
+                ship.horiz = direction[None][0] * speed  ##일시정지 오류해결##
+                ship.vert = direction[None][1] * speed
                 pauseMenu = True
                 menuDict = {1: resumePos, 2: hiScorePos, 3: fxPos, 4: musicPos, 5: quitPos_pause,
                             6: languagePos_pause}
@@ -720,6 +723,8 @@ def main():
                     for txt, pos in textOverlays:
                         screen.blit(txt, pos)
                     pygame.display.flip()
+        
+
 
         if Missile_on == True and time%Missile_gap == 0:
             Missile.position(ship.rect.midtop)
